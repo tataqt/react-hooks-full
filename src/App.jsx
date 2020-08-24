039f3fc7bca868bc861b4404ddddf4ac12be9dfb
tataqt <tata_98@ukr.net>
2020-08-24 13:20:13 +0300

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
-import React, { useState, useMemo, useEffect } from 'react';
-
-function complexComputed(number) {
-  let i = 0;
-  while (i < 1000000000) i++
-  return number * 2
-}
+import React, { useState } from 'react';
 
 function App() {
   const [number, setNumber] = useState(42);
   const [colored, setColored] = useState(false);
 
-  const computed = useMemo(() => complexComputed(number), [number]);
-  const styles = useMemo(() => ({
+  const styles = {
     color: colored ? 'darkred' : 'black'
-  }), [colored])
-
-  useEffect(() => {
-    console.log('Styles changed');
-  }, [styles])
+  }
 
   return (
-    <>
-      <h1 style={styles}>Счетчик: {computed}</h1>
+    <div>
+      <h1 style={styles}>Счетчик: {number}</h1>
       <button onClick={() => setNumber(prev => prev + 1)} className='btn btn-success'>Добавить</button>
-      <button onClick={() => setNumber(prev => prev - 1)} className='btn btn-danger'>Убрать</button>
       <button onClick={() => setColored(prev => !prev)} className='btn btn-warning'>Изменить</button>
-    </>
+    </div>
   );
 }
 
