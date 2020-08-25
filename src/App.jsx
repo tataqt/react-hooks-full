78114ab7e117a1be21baf7cb0e7552dcf9b8c20f
tataqt <tata_98@ukr.net>
2020-08-25 12:49:11 +0300

    custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
-import Main from './Main';
-import Alert from './alert/Alert';
-import { AlertProvider } from './alert/AlertContext';
+import React, { useState, useEffect } from 'react';
+
+function useLogger(value) {
+  useEffect(() => {
+    console.log('Value changed', value);
+  }, [value])
+}
+
+function useInput(initalValue) {
+  const [value, setValue] = useState(initalValue);
+
+  const onChange = event => setValue(event.target.value);
+
+  const clear = () => setValue('')
+
+  return {
+    bind: { value, onChange },
+    value,
+    clear
+  }
+}
 
 function App() {
+  const name = useInput('');
+  const lastName = useInput('');
+
+  useLogger(name.value)
+
   return (
-    <AlertProvider >
-      <div className={'container pt-3'}>
-        <Alert />
-        <Main toggle={() => { }} />
-      </div>
-    </AlertProvider>
+    <div className={'container pt-3'}>
+      <input type="text" {...name.bind} />
+      <button className="btn btn-warning" onClick={name.clear}>Очистить</button><br />
+      <input type="text" {...lastName.bind} />
+      <button className="btn btn-warning" onClick={lastName.clear}>Очистить</button><br />
+      <hr />
+      <h1>{name.value} {lastName.value}</h1>
+    </div>
+
   );
 }
 
